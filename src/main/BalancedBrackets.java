10220cbad51cf3cfcc3ab0097591016f1699e6e3
deltafiveoh <75449438+deltafiveoh@users.noreply.github.com>
2021-05-06 19:56:57 -0500

    junit studio:

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,6 +1,8 @@
 package main;
 
 
+import java.util.ArrayList;
+
 public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
@@ -22,14 +24,50 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
+//        ArrayList<Character> bracketList = new ArrayList<>();
+//
+//        for (int x = 0; x < str.length(); x++){
+//            char[] strArr = str.toCharArray();
+//            if (strArr[x] == '[' || strArr[x] == ']'){
+//                bracketList.add(strArr[x]);
+//            }
+//        }
+//
+//        while (bracketList.size() > 0){
+//            int closer = bracketList.indexOf(']');
+//            if (bracketList.get(closer-1)=='['){
+//                bracketList.remove(closer);
+//                bracketList.remove(closer-1);
+//            } else if (bracketList.get(closer-1)==']' || bracketList.indexOf(']')==0){
+//                return false;
+//            }
+//        }
+//
+//        return bracketList.size() == 0;
+
         int brackets = 0;
+        int openIndex = 0;
+        int closeIndex = 0;
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
                 brackets++;
-            } else if (ch == ']') {
+                openIndex += str.indexOf("[");
+            }
+            if (ch == ']') {
                 brackets--;
+                closeIndex += str.indexOf("]");
             }
         }
-        return brackets == 0;
+
+        if (openIndex <= closeIndex){
+            return brackets == 0;
+        } else {
+            return false;
+        }
+
+
     }
+
+
+
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,53 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+    //TODO: add tests
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrueBasic() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void onlyBracketsReturnsFalseBasic() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueWithNonBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]}"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueWithWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch[Code]]"));
+    }
+
+
+    @Test
+    public void onlyBracketsReturnsTrueEmpty() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalseWrongOrder() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsMultiplePairs() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][[[]]]"));
+    }
 
 }
